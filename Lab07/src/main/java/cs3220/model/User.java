56e4cf53868b59fa2fc0bcef1aafb88955b35e12
benSauce <77858815+benSauce@users.noreply.github.com>
2024-10-08 20:47:10 -0700

    completed user class

--- Lab07/src/main/java/cs3220/model/User.java
+++ Lab07/src/main/java/cs3220/model/User.java
@@ -1,14 +1,53 @@
 package cs3220.model;
 
 public class User {
-	
+
+	static int idSeed = 1;
+
 	private int id;
-	
+
 	private String name;
 	private String username;
 	private char[] password;
-	
-	
 
-	
+	public User(String name, String username, char[] password) {
+		this.id = idSeed++;
+		this.name = name;
+		this.username = username;
+		this.password = password;
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public char[] getPassword() {
+		return password;
+	}
+
+	public void setPassword(char[] password) {
+		this.password = password;
+	}
+
 }
